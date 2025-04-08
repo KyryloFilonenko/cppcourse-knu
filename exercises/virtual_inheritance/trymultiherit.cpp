283e0b2defdf77d112c231a3db79b1ab41b13f05
kyrylo <ababagalamagat1@gmail.com>
2025-04-09 00:55:42 +0300

    virtual_inheritance

--- exercises/virtual_inheritance/trymultiherit.cpp
+++ exercises/virtual_inheritance/trymultiherit.cpp
@@ -3,12 +3,17 @@
 
 int main() {
     // create a TextBox and call draw
-
+    TextBox mybox("my text box", 10, 5);
+    // mybox.draw();
+    // error C2385: ambiguous access of 'draw'
 
     // Fix the code to call both draws by using types
-
+    Rectangle &rec = mybox;
+    rec.draw();
+    Text &tex = mybox;
+    tex.draw();
 
     // try with virtual inheritance
-
+    mybox.draw();
 
 }
