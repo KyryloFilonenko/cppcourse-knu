d214f07ad32250a8a3c02bfb4d40c2d8bc800409
Kyrylo Filonenko <ababagalamagat1@gmail.com>
2025-04-23 00:51:19 +0300

    templates

--- exercises/templates/OrderedVector.hpp
+++ exercises/templates/OrderedVector.hpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include <stdexcept>
 
-template<typename ElementType>
+template<typename ElementType, typename Compare = std::less<ElementType>>
 class OrderedVector {
 public:
     OrderedVector(unsigned int maxLen)
@@ -26,17 +26,18 @@ public:
 private:
     unsigned int m_len = 0;
     unsigned int m_maxLen;
+    Compare comp;
     std::unique_ptr<ElementType[]> m_data;
 };
 
-template<typename ElementType>
-bool OrderedVector<ElementType>::add(ElementType value) {
+template<typename ElementType, typename Compare>
+bool OrderedVector<ElementType, Compare>::add(ElementType value) {
     if (m_len >= m_maxLen) {
         return false;
     }
     // find insertion point
     unsigned int insertIndex = 0;
-    while (insertIndex < m_len && m_data[insertIndex] < value)
+    while (insertIndex < m_len && comp(m_data[insertIndex], value))
         insertIndex++;
     // move end of vector
     unsigned int index = m_len;
--- exercises/templates/playwithsort.cpp
+++ exercises/templates/playwithsort.cpp
@@ -8,12 +8,17 @@
 struct ReverseStringLess {
     bool operator() (const std::string &s, const std::string &t) const {
         // TODO: compare reversed strings
-        // hint: you can use:
-        // - std::views::reverse
-        // - std::ranges::lexicographical_compare
-        // or, if your compiler does not support those yet,
-        // take copies of the strings and reverse them using std::reverse
-        return s < t;
+        std::string ra = s, rb = t;
+        std::reverse(ra.begin(), ra.end());
+        std::reverse(rb.begin(), rb.end());
+        return ra < rb;
+    }
+};
+
+struct ManhattanLess {
+    bool operator()(const Complex& a, const Complex& b) const {
+        return (std::abs(a.real()) + std::abs(a.imaginary())) <
+               (std::abs(b.real()) + std::abs(b.imaginary()));
     }
 };
 
@@ -38,14 +43,43 @@ int main() {
     std::cout << "\n\n";
 
     // TODO: Demonstrate OrderedVector with Complex as element type similar to above
+    std::cout << "Complex\n";
+    OrderedVector<Complex> vc(5);
+    vc.add(Complex(1, 2));
+    vc.add(Complex(3, 4));
+    vc.add(Complex(0.5, 1.5));
+    vc.add(Complex(2, 1));
+    vc.add(Complex(1, 1));
+    for (int i = 0; i < 5; i++)
+        std::cout << vc[i] << " ";
+    std::cout << "\n\n";
 
 
     // TODO: Extend OrderedVector to allow to customize the ordering via an additional template paramter.
     //       Then, demonstrate the new functionality by ordering an OrderedVector<std::string>,
     //       where the strings are compared starting at their last letters.
-
+    std::cout << "Reverse string order\n";
+    OrderedVector<std::string, ReverseStringLess> revVec(5);
+    revVec.add("one");
+    revVec.add("two");
+    revVec.add("three");
+    revVec.add("four");
+    revVec.add("five");
+    for (int i = 0; i < 5; i++)
+        std::cout << revVec[i] << " ";
+    std::cout << "\n\n";
 
     // TODO: Order an OrderedVector of Complex based on the Manhattan distance
+    std::cout << "Complex ordered by Manhattan distance\n";
+    OrderedVector<Complex, ManhattanLess> manhattanVec(5);
+    manhattanVec.add(Complex(3, 4));
+    manhattanVec.add(Complex(1, -1));
+    manhattanVec.add(Complex(0, 0));
+    manhattanVec.add(Complex(-2, 3));
+    manhattanVec.add(Complex(1, 1));
+    for (int i = 0; i < 5; i++)
+        std::cout << manhattanVec[i] << " ";
+    std::cout << "\n\n";
 
 
 }
