db1997e730ccfe06ff4b801c598846849b44440b
Kyrylo Filonenko <ababagalamagat1@gmail.com>
2025-05-14 01:48:57 +0300

    stl

--- exercises/stl/randomize.cpp
+++ exercises/stl/randomize.cpp
@@ -11,17 +11,24 @@ void compute(int len, T initial, T step) {
     std::vector<T> v(len+1), diffs(len+1);
 
     // fill and randomize v
-    std::generate(, , [](...) { ...; });
-    std::shuffle(..., std::default_random_engine{});
+    std::generate(v.begin(), v.end(), [n = 0, initial, step]() mutable {
+        return initial + step * n++;
+    });
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(v.begin(), v.end(), gen);
 
     // compute differences
-    std::adjacent_difference(...);
+    std::adjacent_difference(v.begin(), v.end(), diffs.begin());
+    diffs.erase(diffs.begin());
 
     // compute standard deviation of all differences
-    const T sum = std::reduce(...);
-    const T sumsq = std::accumulate(..., [](...) { ...; });
-    const T mean = sum/len;
-    const T variance = sumsq/len - mean*mean;
+    T sum = std::accumulate(diffs.begin(), diffs.end(), T{});
+    T sumsq = std::accumulate(diffs.begin(), diffs.end(), T{}, [](T acc, T val) {
+        return acc + val * val;
+    });
+    T mean = sum / static_cast<double>(len);
+    T variance = sumsq / static_cast<double>(len) - mean * mean;
 
     std::cout << "Range = [" << initial << ", " << step*len << "]\n"
               << "Mean = " << mean << '\n'
@@ -29,6 +36,9 @@ void compute(int len, T initial, T step) {
 }
 
 int main() {
-    compute(1000, 0.0, 7.0);
+    std::cout << "=== DOUBLE ===\n";
+    compute(100, 0.0, 7.0);
     // call compute here with Complex
+    std::cout << "=== COMPLEX ===\n";
+    compute(100, Complex(0, 0), Complex(7, 0));
 }
